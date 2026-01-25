fa9a9ce665e2d1a92077bc81803e0c630140d2a3
Kelvin <shelvinbrown@gmail.com>
2026-01-25 22:05:20 +0000

    navbar update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,37 +5,22 @@ import { Link } from "react-router";
 const Sidebar = () => {
   return (
     <>
-      <navbar className="w-56 h-screen flex flex-col ">
+      <nav className="w-56 h-screen flex flex-col ">
         {/* top section  */}
         <div className="flex gap-5 justify-center items-center">
           <CalendarDays
             size={40}
-            color="gray"
-            className="bg-amber-400 rounded-xl p-2"
+            color="yellow"
+            className="bg-black/80 rounded-xl p-2"
           />
           <p className="text-2xl font-bold w-40">Origin8's leave Management</p>
         </div>
         <ul>
-          {/* <li>
-            <Link to=""> Dashboard</Link>
-          </li>
           <li>
-            <Link to="">Apply Leave</Link>
+            <a></a>
           </li>
-          <li>
-            <Link to="" My Leaves></Link>
-          </li>
-          <li>
-            <Link to=""> My Profile</Link>
-          </li>
-          <li>
-            <Link to="">Approve Leaves</Link>
-          </li>
-          <li>
-            <Link to="">Employees</Link>
-          </li> */}
         </ul>
-      </navbar>
+      </nav>
     </>
   );
 };
